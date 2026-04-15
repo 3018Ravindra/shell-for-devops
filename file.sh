8a2f6325890c29b1d3450f77319a2e2baa8d7518
3018Ravindra <veerravindra190@gmail.com>
2026-04-15 16:14:25 +0530

    working in git bash

--- file.sh
+++ file.sh
@@ -8,4 +8,4 @@ echo "Welcome to Devops"
 # then run the file using ./file.sh
 
 #!/bin/bash is called shebang and it tells the system which interpreter to use to execute the script. In this case, it specifies that the script should be run using the bash shell.
-echo is a command used to display a message or output to the terminal. In this script, it is used to print "Hello Ravindra" and "Welcome to Devops" to the console.
+# echo is a command used to display a message or output to the terminal. In this script, it is used to print "Hello Ravindra" and "Welcome to Devops" to the console.
