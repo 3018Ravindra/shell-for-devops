537e6afc54257e847eae3c321a8a6570bba7ac2e
3018Ravindra <veerravindra190@gmail.com>
2026-04-15 15:38:46 +0530

    one more line

--- file.sh
+++ file.sh
@@ -6,3 +6,6 @@ echo "Welcome to Devops"
 # next step should be
 # saving file ctrl+s 
 # then run the file using ./file.sh
+
+#!/bin/bash is called shebang and it tells the system which interpreter to use to execute the script. In this case, it specifies that the script should be run using the bash shell.
+echo is a command used to display a message or output to the terminal. In this script, it is used to print "Hello Ravindra" and "Welcome to Devops" to the console.
