4ecd4af6df86d295b49a32d485e946ef5288a043
3018Ravindra <veerravindra190@gmail.com>
2026-04-15 15:35:42 +0530

    some instructions for shell scripting

--- file.sh
+++ file.sh
@@ -3,25 +3,6 @@
 echo "Hello Ravindra"
 echo "Welcome to Devops"
 
-chmod +x file.sh
-./file.sh
-
-
-#!/bin/bash
-
-name="Ravindra"
-age=25
-
-echo "My name is $name"
-echo "My age is $age"
-
-# example of conditional statement
-
-#!bin/bash
-
-age=23
-
-if [ $age -gt 18 ]
-then
-    echo "You are an adult"
-fi
+# next step should be
+# saving file ctrl+s 
+# then run the file using ./file.sh
